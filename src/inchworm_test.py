a52229694ba2ace6a84d0d83c29b619e4bb20d3a
Rich Dobbs <rich.dobbs.13440@gmail.com>
2023-05-24 01:16:31 -0600

    Able to send command to the arduino, but not reading response

--- src/inchworm_test.py
+++ src/inchworm_test.py
@@ -4,8 +4,8 @@ import serial
 
 def send_command_to_arduino(command):
     # Configure the serial port
-    port = '/dev/ttyACM0 '  # Change this to match your Arduino's port
-    baud_rate = 115200  # Set the baud rate to match your Arduino's configuration
+    port = '/dev/ttyACM0'  # Change this to match your Arduino's port
+    baud_rate = 9600  # Set the baud rate to match your Arduino's configuration
 
     # Open the serial port
     ser = serial.Serial(port, baud_rate)
